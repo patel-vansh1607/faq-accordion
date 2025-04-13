5b466abae90005f2454d1349afc7a118928624ba
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-13 21:15:06 +0300

    del and new fucntion

---
+++ src/components/Accordion.js
@@ -0,0 +1,11 @@
+const FAQAccordion = () =>{
+    return(
+        <div className="main-div">
+            <div className="accordion">
+
+            </div>
+        </div>
+    )
+}
+
+export default FAQAccordion;
